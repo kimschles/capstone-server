5532dcf4adaf0165f268bf40482d63241521d49e
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-09-28 12:50:03 -0600

    refactor

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,9 @@
+require('dotenv').load()
+
 var express = require('express');
 var router = express.Router();
 var unirest = require('unirest');
-var db = require('monk')('localhost/capstone-demo');
+var db = require('monk')(process.env.MONGOLAB_URI);
 var searchTerms = db.get('searchTerms');
 
 router.get('/', function(req, res) {});
@@ -15,6 +17,15 @@ router.post('/', function(req, res) {
   });
 });
 
+router.get('/latestsearches', function(req, res) {
+  searchTerms.find({ $query: {}, $orderby: { _id: -1 } }, function(err, search) {
+    if (err) {
+      res.send(err);
+    }
+    res.status(200).json(search);
+  })
+});
+
 // parameter middleware that will run before the '/list/:state' route
 router.param('state', function(req, res, next, state) {
     req.state = state;
@@ -22,63 +33,50 @@ router.param('state', function(req, res, next, state) {
 });
 
 router.get('/list/:state', function(req, res, next) {
-  unirest.get('http://www.opensecrets.org/api/?method=getLegislators&id=' + req.state + '&apikey=93167543cdcbf60453604935fe238344&output=json')
+  unirest.get('http://www.opensecrets.org/api/?method=getLegislators&id=' + req.state + '&apikey='+ process.env.OPEN_SECRETS_KEY +'&output=json')
     .end(function (response) {
       res.status(200).json(response)
     });
 });
 
 // parameter middleware that will run before the '/show/:cid' route
-// router.param('cid', function(req, res, next, cid) {
-//     console.log(cid);
-//     req.cid = cid;
-//     next();
-// });
+router.param('cid', function(req, res, next, cid) {
+    req.cid = cid;
+    next();
+});
 
-router.get('/show/', function(req, res, next) {
-  unirest.get('http://www.opensecrets.org/api/?method=candSummary&cid=N00006134&cycle=2016&apikey=93167543cdcbf60453604935fe238344&output=json')
+router.get('/show/:cid', function(req, res, next) {
+  unirest.get('http://www.opensecrets.org/api/?method=candSummary&cid=' + req.cid + '&cycle=2016&apikey='+ process.env.OPEN_SECRETS_KEY + '&output=json')
     .end(function (response) {
       res.status(200).json(response)
     });
 });
 
-router.get('/topContributor/', function(req, res, next) {
-  unirest.get('http://www.opensecrets.org/api/?method=candContrib&cid=N00006134&cycle=2016&apikey=93167543cdcbf60453604935fe238344&output=json')
+router.get('/topContributor/:cid', function(req, res, next) {
+  unirest.get('http://www.opensecrets.org/api/?method=candContrib&cid=' + req.cid +'&cycle=2016&apikey='+ process.env.OPEN_SECRETS_KEY + '&output=json')
   .end(function (response) {
     res.status(200).json(response)
   });
 });
 
-router.get('/topSector/', function(req, res, next) {
-  unirest.get('http://www.opensecrets.org/api/?method=candSector&cid=N00007360&cycle=2016&apikey=93167543cdcbf60453604935fe238344&output=json')
+router.get('/topSector/:cid', function(req, res, next) {
+  unirest.get('http://www.opensecrets.org/api/?method=candSector&cid=' + req.cid + '&cycle=2016&apikey='+ process.env.OPEN_SECRETS_KEY + '&output=json')
   .end(function (response) {
     res.status(200).json(response)
   });
 });
 
-router.get('/topsearches', function(req, res) {
-  searchTerms.find({}, function(err, search) {
-    if (err) {
-      res.send(err);
-    }
-    res.status(200).json(search);
-  })
+router.param('lastname', function(req, res, next, lastname) {
+    req.lastname = lastname.toLowerCase();
+    next();
 });
 
-router.get('/', function(req, res) {
-  searchTerms.find({}, function(err, search) {
-    if (err) {
-      res.send(err);
-    }
-    res.status(200).json(search);
-  })
-});
-
-router.get('/stories/', function(req, res, next) {
-  unirest.get('https://webhose.io/search?token=33327263-0aa8-4fa4-bf15-7a7ef7594a6d&format=json&q=Congress%20%22Jared%20Polis%22&language=english&ts=1442961669993')
+router.get('/stories/:lastname', function(req, res, next) {
+  unirest.get('https://webhose.io/search?token=' + process.env.WEBHOSE_KEY + '=json&q=Congress%20%22' + req.lastname + '%22%20text%3A('+ req.lastname +')&language=english&site_type=news')
     .end(function (response) {
       res.status(200).json(response)
     });
 });
+
 
 module.exports = router;
