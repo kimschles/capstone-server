4caed3b42ccc3398670ba4fbc7a411e44fa045fd
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-10-04 17:36:17 -0600

    debugging for cloud foundry deploy

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.use(bodyParser.json());
 app.use('/api/financeData', financeData);
 app.use('/api/searchTerms', searchTerms);
 
-app.listen(process.env.PORT || 1985);
-console.log('Yaassss. Server started on localhost://1985');
+app.listen(process.env.PORT);
+console.log('Yaassss. Server started!');
