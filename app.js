d7bda065711d1264960369b4994f4edb68c15315
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-11-15 09:07:35 -0700

    allowing client side access

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/api/financeData', financeData);
 app.use('/api/searchTerms', searchTerms);
