7fcf34133c1ed2d348d0f251fca51153420e5a4d
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-11-16 14:03:26 -0700

    CORS

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var financeData = require('./routes/routes');
 var searchTerms = require('./routes/routes');
+var cors = require('cors');
+
 
 var app = express();
 
-var cors = require('cors');
 app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
